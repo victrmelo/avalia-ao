c0001dc11cea74976d572c5393e917a95dc9c020
victrmelo <161876325+victrmelo@users.noreply.github.com>
2024-11-22 13:37:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('voltar').addEventListener('click', function () {
+        window.location.href = 'index.html';
+    });
+});
